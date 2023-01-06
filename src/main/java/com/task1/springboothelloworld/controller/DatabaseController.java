9dccffc47dfd3da1ad368bf261a85c78e891c2c5
Edin Bajrić <bedin@comtrade.com>
2023-01-06 14:14:16 +0100

    added exception handling

--- src/main/java/com/task1/springboothelloworld/controller/DatabaseController.java
+++ src/main/java/com/task1/springboothelloworld/controller/DatabaseController.java
@@ -2,6 +2,8 @@ package com.task1.springboothelloworld.controller;
 
 import com.task1.springboothelloworld.entity.Message;
 import com.task1.springboothelloworld.service.MessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,9 @@ import javax.validation.Valid;
 @RequestMapping("/api/db")
 
 public class DatabaseController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseController.class);
+    private final String errorMessage = "The length of the message must be at least 5 characters long and/or your message contains invalid characters";
 
     @Autowired
     private final MessageService messageService;
@@ -70,6 +75,16 @@ public class DatabaseController {
     public String returnHello() {
 
         return "Hello from db!";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleValidationException(Exception ex) {
+        logger.error(ex.getMessage());
+        ModelAndView modelAndView = new ModelAndView("error");
+
+        modelAndView.addObject("message", errorMessage);
+
+        return modelAndView;
     }
 }
 
